05eec487e347d4d36f385deaaa692cd10250124a
Matthew Huie <mphuie@gmail.com>
2015-09-09 20:40:37 +0000

    First commit

---
+++ getCheckedOut.py
@@ -0,0 +1,39 @@
+import requests
+from bs4 import BeautifulSoup
+import json
+from dotenv import load_dotenv
+import os
+
+load_dotenv(".env")
+
+s = requests.Session()
+
+r = s.get("https://kcls.bibliocommons.com/user/login", verify=False)
+
+payload = {
+    "name": os.environ.get("USER"),
+    "user_pin": os.environ.get("PIN")
+}
+
+s.post("https://kcls.bibliocommons.com/user/login", data=payload)
+
+r = s.get("https://kcls.bibliocommons.com/checkedout?display_quantity=50&page=1&view=medium")
+soup = BeautifulSoup(r.text, "html.parser")
+
+checkedOutList = soup.find("div", { "id": "bibList" })
+
+checkedOutItems = []
+
+for title in checkedOutList.find_all("div", { "class": "listItem" }):
+    title_name = title.find("span", { "class": "title" })
+    due_date = title.find("span", { "class": "item_due_date" })
+    
+    checkedOutItems.append({ "title": title_name.text.strip(), "due": due_date.text.strip() })
+    
+
+
+
+with open("checkedout.json", "w") as f:
+    print "%d title(s) checked out" % len(checkedOutItems)
+    f.write(json.dumps(checkedOutItems))
+    
