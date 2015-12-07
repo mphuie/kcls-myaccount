da2096eea16c93668a8233d274670e73e438117c
Matthew Huie <mphuie@gmail.com>
2015-12-07 14:48:47 -0800

    Updated scraper

--- getCheckedOut.py
+++ getCheckedOut.py
@@ -16,18 +16,18 @@ payload = {
 }
 
 p = s.post("https://kcls.bibliocommons.com/user/login", data=payload)
-r = s.get("https://kcls.bibliocommons.com/checkedout?display_quantity=50&page=1&view=medium")
+r = s.get("https://kcls.bibliocommons.com/checkedout/index/out")
 soup = BeautifulSoup(r.text, "html.parser")
-checkedOutList = soup.find("div", { "id": "bibList" })
+checkedOutList = soup.find("div", { "class": "cp_bib_list" })
 
 checkedOutItems = []
 
 for title in checkedOutList.find_all("div", { "class": "listItem" }):
     title_name = title.find("span", { "class": "title" })
-    due_date = title.find("span", { "class": "item_due_date" })
-    
+    due_date = title.find("span", { "class": "checkedout_status" })
+
     checkedOutItems.append({ "title": title_name.text.strip(), "due": due_date.text.strip() })
-    
+
 with open("checkedout.json", "w") as f:
     print "%d title(s) checked out" % len(checkedOutItems)
     f.write(json.dumps(checkedOutItems))
